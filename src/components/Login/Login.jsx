0c0f06983efd2d30202c159c5b9ac72fed1fe665
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-19 22:57:48 +0530

    create login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Login = () => {
     return (
-        <div>
+        <div className='text-center'>
             <h2>Login coming sooon</h2>
+            <form>
+                <div>
+                <h2>Email</h2>
+                <input type="email" />
+                </div>
+                <div>
+               <h2>Password</h2>
+               <input type="password" />
+                </div>
+            </form>
         </div>
     );
 };
